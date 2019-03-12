2ee90ddde6ed08c6d1cb0d988fa80c392277f0ed
filipvlais <exzoneyt@gmail.com>
2019-03-12 22:21:55 +0100

    Verzija za rad

--- main.cpp
+++ main.cpp
@@ -1,8 +1,61 @@
-#include <iostream>
+/**
+ * @file main.cpp
+ * @author Edah Sijerčić, Filip Vlaisavljević 
+ * @brief 
+ * @version 1.0
+ * @date 12.03.2019
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 
-int main(){
+#include <bits/stdc++.h> 
+#include <string>
 
-    std::cout<<"0101"<<std::endl;
+using namespace std; 
 
-    return 0;
+string textGenerator(){
+
+ /*
+ Napraviti funkciju koja uzima text iz fajla. 
+ Svaki zadatak će biti spašen u .txt fileu i formatiran
+ kao LaTeX. Program će uzimati random .txt fajlove te ih
+ spasiti kao zadatke u jedan .HTML fajl
+
+ Onda možemo, konvertovati .HTML u .pdf, ali prvo implementirajmo
+ čitanje .txt file-a i njegovo konvertovanje u .html
+
+ */
+
 }
+
+  
+int main() 
+{ 
+
+   fstream file; 
+
+  /* Generisanje .HTML file-a */
+
+   file.open("index.html",ios::out); 
+
+  /* Default ERROR linija */
+
+   if(!file) 
+   { 
+       cout<<"Greska u pravljenu fajla!"; 
+       return 0; 
+   } 
+
+ /* Default linija nakon generisanja */
+  
+   cout<<"Uspjesno ste generisali fajl!"; 
+
+ /* Funkcija upisuje text u .html file */
+ 
+   file<<textGenerator();
+
+   file.close(); 
+  
+   return 0; 
+} 
